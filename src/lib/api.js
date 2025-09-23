ec1010973d5161a0b982044c282fad8e6441bd4b
Homa-x <homayoun3815@yahoo.com>
2025-09-23 22:24:20 +0200

    fix: switch to CJS Netlify function proxy

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,17 +1,6 @@
 import axios from "axios";
-
-const api = axios.create({
-  baseURL: "/api",
-  withCredentials: false, 
-});
-
-api.interceptors.request.use((config) => {
-  const jwt = localStorage.getItem("token");
-  if (jwt) config.headers.Authorization = `Bearer ${jwt}`;
-  return config;
-});
-
-export async function getCSRF() { return true; }
-export async function ensureCSRF() { return true; }
-
+const api = axios.create({ baseURL: "/api", withCredentials: false });
+api.interceptors.request.use((cfg)=>{ const t=localStorage.getItem("token"); if(t) cfg.headers.Authorization=`Bearer ${t}`; return cfg;});
+export async function getCSRF(){ return true; }
+export async function ensureCSRF(){ return true; }
 export default api;
